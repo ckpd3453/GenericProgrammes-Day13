e7db18eb0f1403f1e325019fbdf79deafa4f202c
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-02-28 17:26:52 +0530

    Added Test Case 1 to find maximum number at first position

--- src/maximum/FloatMaximum.java
+++ src/maximum/FloatMaximum.java
@@ -1,18 +1,18 @@
 package maximum;
 
-public class Maximum {
+public class FloatMaximum {
 
 	public static void main(String[] args) {
 
-		calMax(20, 40, 50); // passing values to calMax method
+		calMax(80.8f, 20.5f, 50.8f); // passing values to calMax method
 
 	}
 
 	/**
 	 * method to calculate maximum no.
 	 */
-	public static Integer calMax(Integer a, Integer b, Integer c) {
-		Integer max = a;
+	public static Float calMax(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(max) > 0) {
 			max = b;
 		}
@@ -27,7 +27,7 @@ public class Maximum {
 	/*
 	 * method to print
 	 */
-	private static void displayMax(Integer a, Integer b, Integer c, Integer max) {
+	private static void displayMax(Float a, Float b, Float c, Float max) {
 		System.out.println("***********To Find Maximum number*********");
 		System.out.println("The Given Values are:- \na = " + a + "\nb = " + b + "\nc = " + c);
 		System.out.println("Maximum No. is: " + max);
