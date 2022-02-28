a928a1ccddb7d40605f697882ee52abe9a1642da
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-02-28 17:02:14 +0530

    Added Test Case-1.1 to find maximum no. at first position

---
+++ src/maximum/Maximum.java
@@ -0,0 +1,36 @@
+package maximum;
+
+public class Maximum {
+
+	public static void main(String[] args) {
+
+		calMax(20, 10, 5); // passing values to calMax method
+
+	}
+
+	/**
+	 * method to calculate maximum no.
+	 */
+	public static Integer calMax(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0) {
+			max = b;
+		}
+		if (c.compareTo(max) > 0) {
+			max = c;
+		}
+		displayMax(a, b, c, max);
+		return max;
+
+	}
+
+	/*
+	 * method to print
+	 */
+	private static void displayMax(Integer a, Integer b, Integer c, Integer max) {
+		System.out.println("***********To Find Maximum number*********");
+		System.out.println("The Given Values are:- \na = " + a + "\nb = " + b + "\nc = " + c);
+		System.out.println("Maximum No. is: " + max);
+	}
+
+}
