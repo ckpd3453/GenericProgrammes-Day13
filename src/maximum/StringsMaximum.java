ce13d15f5ba694aedfd5879a26a63fba665ce768
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-02-28 17:50:52 +0530

    Added Test Case 1 to find maximum string at first position

--- src/maximum/StringsMaximum.java
+++ src/maximum/StringsMaximum.java
@@ -1,18 +1,18 @@
 package maximum;
 
-public class FloatMaximum {
+public class StringsMaximum {
 
 	public static void main(String[] args) {
 
-		calMax(45.8f, 40.5f, 50.8f); // passing values to calMax method
+		calMax("Bittu","Akash","Anand"); // passing values to calMax method
 
 	}
 
 	/**
 	 * method to calculate maximum no.
 	 */
-	public static Float calMax(Float a, Float b, Float c) {
-		Float max = a;
+	public static String calMax(String a, String b, String c) {
+		String max = a;
 		if (b.compareTo(max) > 0) {
 			max = b;
 		}
@@ -27,7 +27,7 @@ public class FloatMaximum {
 	/*
 	 * method to print
 	 */
-	private static void displayMax(Float a, Float b, Float c, Float max) {
+	private static void displayMax(String a, String b, String c, String max) {
 		System.out.println("***********To Find Maximum number*********");
 		System.out.println("The Given Values are:- \na = " + a + "\nb = " + b + "\nc = " + c);
 		System.out.println("Maximum No. is: " + max);
