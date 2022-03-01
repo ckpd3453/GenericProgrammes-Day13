431fb35ae3722e0f4cfddb4579a5acd5a1b1dd6d
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-01 06:00:31 +0530

    Modified codes

--- src/maximum/RefactorIntegerMaximum.java
+++ src/maximum/RefactorIntegerMaximum.java
@@ -3,14 +3,22 @@ package maximum;
 public class RefactorIntegerMaximum {
 		
 	public static void main(String[] args) {
+		/**
+		 * took different data types variable as input
+		 */
 		Integer a = 20, b = 40, c = 35;
 		Float p = 35.15f,q = 35.50f,r = 35.25f;
 		String x = "BridgeLabz",y = "Hcl",z = "Accenture";
 		
+		//Calling method
 		toCal(a,b,c);
 		toCal(p,q,r);
 		toCal(x,y,z);
 	}
+
+	/**
+	 * Method to calculate maimum value
+	*/
 	public static <S extends Comparable<S>> S toCal(S a,S b,S c)
 	{
 		S max = a;
@@ -22,7 +30,12 @@ public class RefactorIntegerMaximum {
 		{
 			max = c;
 		}
-		System.out.printf("\nGiven values are: %s %s %s", a, b, c");"
-		System.out.println("\nMaximum Value: "+max);
+		toPrints(max);
+	}
+	
+	//Method to print output
+	public static <S> void toPrints(S a,S b,S c,S max) {
+		System.out.printf("\nGiven values are: %s %s %s", a, b, c")"
+				System.out.println("\nMaximum Value: "+max);
 	}
 }
