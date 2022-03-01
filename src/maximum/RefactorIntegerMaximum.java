531ad93f125649275bf6a19ee4652186372c10b9
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-01 05:50:40 +0530

    Added Refactor Problem-1

--- src/maximum/RefactorIntegerMaximum.java
+++ src/maximum/RefactorIntegerMaximum.java
@@ -2,14 +2,27 @@ package maximum;
 
 public class RefactorIntegerMaximum {
 		
-
 	public static void main(String[] args) {
 		Integer a = 20, b = 40, c = 35;
 		Float p = 35.15f,q = 35.50f,r = 35.25f;
 		String x = "BridgeLabz",y = "Hcl",z = "Accenture";
 		
-		CalMaximum.<Integer>calMax(a , b , c);
-		CalMaximum.<Float>calMax(p , q , r);
-		CalMaximum.<String>calMax(x , y , z);
+		toCal(a,b,c);
+		toCal(p,q,r);
+		toCal(x,y,z);
+	}
+	public static <S extends Comparable<S>> S toCal(S a,S b,S c)
+	{
+		S max = a;
+		if(b.compareTo(max)>0)
+		{
+			max=b;
+		}
+		if(c.compareTo(max)>0)
+		{
+			max = c;
+		}
+		System.out.printf("\nGiven values are: %s %s %s", a, b, c");"
+		System.out.println("\nMaximum Value: "+max);
 	}
 }
